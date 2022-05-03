1b90e4d0b16eecb6af01b31c0c3b98492a34c7a2
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-05-03 19:27:34 +0200

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { TestComponent } from './test/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
